88caddf50ab75de3359e25eda077a5c16157fb6f
yevgeller <yevgeller@gmail.com>
2023-09-22 22:19:50 -0400

    Working on questions and database

---
+++ Hiragana1/Shared/QuestionType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared
+{
+    public enum QuestionType
+    {
+        EnglishToHiragana = 0,
+        EnglishToKatakana,
+        HiraganaToEnglish,
+        KatakanaToEnglish,
+        HiraganaToKatakana,
+        KatakanaToHiragana,
+        Random
+    }
+}
--- QuestionnaireService/QuestionnaireGenerator.cs
+++ QuestionnaireService/QuestionnaireGenerator.cs
@@ -1,7 +1,50 @@
-﻿namespace QuestionnaireService
+﻿using Hiragana1.Shared;
+
+namespace QuestionnaireService
 {
     public class QuestionnaireGenerator
     {
 
+        private List<QuestionV1> hiToKa = new List<QuestionV1>();
+        public QuestionnaireGenerator()
+        {
+                hiToKa.Add(new QuestionV1 { QuestionId=1, QuestionType = QuestionType.HiraganaToKatakana, Question= "あ", CorrectAnswer= "ア" });
+        }
+
+        private List<QuestionV1> list = new List<QuestionV1> {
+             new QuestionV1 {
+                 QuestionId = 1,
+                Answers = new[] { "one", "two", "three", "four" }.ToList(),
+                CorrectAnswer = "one",
+                Question = "ichi"
+            },
+ new QuestionV1 {
+     QuestionId = 2,
+                Answers = new[] { "one", "two", "three", "four" }.ToList(),
+                CorrectAnswer = "two",
+                Question = "ni"
+            }, new QuestionV1 {
+                QuestionId = 3,
+                Answers = new[] { "one", "two", "three", "four" }.ToList(),
+                CorrectAnswer = "three",
+                Question = "ichi"
+            }, new QuestionV1 {
+                QuestionId = 4,
+                Answers = new[] { "one", "two", "three", "four" }.ToList(),
+                CorrectAnswer = "one",
+                Question = "ichi"
+            }, new QuestionV1 {
+                QuestionId = 5,
+                Answers = new[] { "one", "two", "three", "four" }.ToList(),
+                CorrectAnswer = "one",
+                Question = "ichi"
+            }
+        };
+        public List<QuestionV1> GetNewTest()
+        {
+
+
+            return new List<QuestionV1>();
+        }
     }
 }
