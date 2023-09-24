9d2661c5bd078c48c86b7639d3338a90a5fb406c
yevgeller <yevgeller@gmail.com>
2023-09-23 23:00:44 -0400

    hiragana string

--- QuestionnaireService/QuestionnaireGenerator.cs
+++ QuestionnaireService/QuestionnaireGenerator.cs
@@ -2,9 +2,11 @@
 
 namespace QuestionnaireService
 {
-    public class QuestionnaireGenerator
-    {
 
+      public class QuestionnaireGenerator
+    {
+string hiragana = "あ い う え お か が き ぎ く ぐ け げ こ ご さ ざ し じ す ず せ ぜ そ ぞ た だ ち ぢ っ つ づ て で と ど な に ぬ ね の は ば ぱ ひ び ぴ ふ ぶ ぷ へ べ ぺ ほ ぼ ぽ ま み む め も ゃ や ゅ ゆ ょ よ ら り る れ ろ ゎ わ ゐ ゑ を ん ゔ ゕ ゖ゛゜ゝゞゟ";
+  
         private List<QuestionV1> hiToKa = new List<QuestionV1>();
         public QuestionnaireGenerator()
         {
