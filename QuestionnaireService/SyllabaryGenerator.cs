ac3c8bbb85f87cd894dc901cfe45b6d1ff1cac80
yevgeller <yevgeller@gmail.com>
2023-09-24 20:18:17 -0400

    Working on new syllabary system

--- QuestionnaireService/SyllabaryGenerator.cs
+++ QuestionnaireService/SyllabaryGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hiragana1.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,18 @@ using System.Threading.Tasks;
 
 namespace QuestionnaireService
 {
-    internal class SyllabaryGenerator
+    public static class SyllabaryGenerator
     {
-        public SyllabaryGenerator()
+        //string hiragana = "あ い う え お か が き ぎ く ぐ け げ こ ご さ ざ し じ す ず せ ぜ そ ぞ た だ ち ぢ っ つ づ て で と ど な に ぬ ね の は ば ぱ ひ び ぴ ふ ぶ ぷ へ べ ぺ ほ ぼ ぽ ま み む め も ゃ や ゅ ゆ ょ よ ら り る れ ろ ゎ わ ゐ ゑ を ん ゔ ゕ ゖ゛゜ゝゞゟ";
+        //string katakana = "ア イ ウ エ オ カ ガ キ ギ ク グ ケ ゲ コ ゴ サ ザ シ ジ ス ズ セ ゼ ソ ゾ タ ダ チ ヂ ッ ツ ヅ テ デ ト ド ナ ニ ヌ ネ ノ ハ バ パ ヒ ビ ピ フ ブ プ ヘ ベ ペ ホ ボ ポ マ ミ ム メ モ ャ ヤ ュ ユ ョ ヨ ラ リ ル レ ロ ヮ ワ ヰ ヱ ヲ ン ヴ ヵ ヶ ヷ ヸ ヹ ヺ ・ ー ヽ ヾ ヿ";
+
+        public static List<SyllabaryCharacter> GetSyllabaryCharacters()
         {
-            
+            List<SyllabaryCharacter> result = new List<SyllabaryCharacter> ();
+            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
+            result.Add(new SyllabaryCharacter { Hiragana = "あ", Katakana = "ア", Transliteration = "a" });
+
+            return result;
         }
     }
 }
