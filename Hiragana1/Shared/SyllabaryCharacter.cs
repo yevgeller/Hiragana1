2c664b2574b5d4c1880b1ca8c6d63bad80851230
yevgeller <yevgeller@gmail.com>
2023-09-24 20:13:57 -0400

    New class for syllabary item

---
+++ Hiragana1/Shared/SyllabaryCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared
+{
+    public class SyllabaryCharacter
+    {
+        public string Hiragana { get; set; }
+        public string Katakana { get; set; }
+        public string Transliteration { get; set; }
+    }
+}
