0dea507d68872151edba2d1b5ee0bbb738d616c3
yevgeller <yevgeller@gmail.com>
2023-09-28 15:02:05 -0400

    syllabary

--- QuestionnaireService/SyllabaryGenerator.cs
+++ QuestionnaireService/SyllabaryGenerator.cs
@@ -11,8 +11,8 @@ namespace QuestionnaireService
     {
         //string hiragana = "あ い う え お か が き ぎ く ぐ け げ こ ご さ ざ し じ す ず せ ぜ そ ぞ た だ ち ぢ っ つ づ て で と ど な に ぬ ね の は ば ぱ ひ び ぴ ふ ぶ ぷ へ べ ぺ ほ ぼ ぽ ま み む め も ゃ や ゅ ゆ ょ よ ら り る れ ろ ゎ わ ゐ ゑ を ん ゔ ゕ ゖ゛゜ゝゞゟ";
         //string katakana = "ア イ ウ エ オ カ ガ キ ギ ク グ ケ ゲ コ ゴ サ ザ シ ジ ス ズ セ ゼ ソ ゾ タ ダ チ ヂ ッ ツ ヅ テ デ ト ド ナ ニ ヌ ネ ノ ハ バ パ ヒ ビ ピ フ ブ プ ヘ ベ ペ ホ ボ ポ マ ミ ム メ モ ャ ヤ ュ ユ ョ ヨ ラ リ ル レ ロ ヮ ワ ヰ ヱ ヲ ン ヴ ヵ ヶ ヷ ヸ ヹ ヺ ・ ー ヽ ヾ ヿ";
-        //string hiragana = "    げ こ ご さ ざ し じ す ず せ ぜ そ ぞ た だ ち ぢ っ つ づ て で と ど な に ぬ ね の は ば ぱ ひ び ぴ ふ ぶ ぷ へ べ ぺ ほ ぼ ぽ ま み む め も ゃ や ゅ ゆ ょ よ ら り る れ ろ ゎ わ ゐ ゑ を ん ゔ ゕ ゖ゛゜ゝゞゟ";
-        //string katakana = "    ゲ コ ゴ サ ザ シ ジ ス ズ セ ゼ ソ ゾ タ ダ チ ヂ ッ ツ ヅ テ デ ト ド ナ ニ ヌ ネ ノ ハ バ パ ヒ ビ ピ フ ブ プ ヘ ベ ペ ホ ボ ポ マ ミ ム メ モ ャ ヤ ュ ユ ョ ヨ ラ リ ル レ ロ ヮ ワ ヰ ヱ ヲ ン ヴ ヵ ヶ ヷ ヸ ヹ ヺ ・ ー ヽ ヾ ヿ";
+        //string hiragana = "ず せ ぜ そ ぞ た だ ち ぢ っ つ づ て で と ど な に ぬ ね の は ば ぱ ひ び ぴ ふ ぶ ぷ へ べ ぺ ほ ぼ ぽ ま み む め も ゃ や ゅ ゆ ょ よ ら り る れ ろ ゎ わ ゐ ゑ を ん ゔ ゕ ゖ゛゜ゝゞゟ";
+        //string katakana = "ズ セ ゼ ソ ゾ タ ダ チ ヂ ッ ツ ヅ テ デ ト ド ナ ニ ヌ ネ ノ ハ バ パ ヒ ビ ピ フ ブ プ ヘ ベ ペ ホ ボ ポ マ ミ ム メ モ ャ ヤ ュ ユ ョ ヨ ラ リ ル レ ロ ヮ ワ ヰ ヱ ヲ ン ヴ ヵ ヶ ヷ ヸ ヹ ヺ ・ ー ヽ ヾ ヿ";
 
         public static List<SyllabaryCharacter> GetSyllabaryCharacters()
         {
@@ -30,14 +30,14 @@ namespace QuestionnaireService
             //result.Add(new SyllabaryCharacter { Hiragana = "く", Katakana = "ク", Transliteration = "" });
             //result.Add(new SyllabaryCharacter { Hiragana = "ぐ", Katakana = "グ", Transliteration = "" });
             //result.Add(new SyllabaryCharacter { Hiragana = "け", Katakana = "ケ", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
-            //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "げ", Katakana = "ゲ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "こ", Katakana = "コ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "ご", Katakana = "ゴ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "さ", Katakana = "サ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "ざ", Katakana = "ザ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "し", Katakana = "シ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "じ", Katakana = "ジ", Transliteration = "" });
+            //result.Add(new SyllabaryCharacter { Hiragana = "す", Katakana = "ス", Transliteration = "" });
             //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
             //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
             //result.Add(new SyllabaryCharacter { Hiragana = "", Katakana = "", Transliteration = "" });
