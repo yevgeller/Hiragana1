6767b86210654ebd6cbd842bb526fa43cd5b9e9f
yevgeller <yevgeller@gmail.com>
2023-10-21 22:18:12 -0400

    Using external library to create tests

--- Hiragana1/Server/Services/QuestionnaireService.cs
+++ Hiragana1/Server/Services/QuestionnaireService.cs
@@ -1,11 +1,13 @@
 ﻿using Hiragana1.Shared;
 using QuestionnaireService;
+using SyllabaryQuizGenerator;
 
 namespace Hiragana1.Server.Services
 {
     public interface IQuestionnaireService
     {
         IEnumerable<QuestionV1> GetNewQuestionnaire(int numberOfQuestions, QuestionnaireType questionType);
+        IEnumerable<QuizItem> GetQuizItems(QuestionnaireType questionType);
     }
     public class QuestionnaireService : IQuestionnaireService
     {
@@ -18,6 +20,12 @@ namespace Hiragana1.Server.Services
         {
             QuestionnaireGenerator generator = new QuestionnaireGenerator();
             return generator.GetNewTest2(testType: QuestionnaireType.EnglishToHiragana, 5);
+        }
+
+        public IEnumerable<QuizItem> GetQuizItems(QuestionnaireType questionType)
+        {
+            QuizGenerator qg = new QuizGenerator();
+            return qg.GenerateQuizItems(10);
         }
     }
 }
