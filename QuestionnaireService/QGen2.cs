6ffdeffddbc27f38723eb272e7969124775c6a6a
yevgeller <yevgeller@gmail.com>
2023-10-21 22:04:46 -0400

    Using external library to generate a test

--- QuestionnaireService/QGen2.cs
+++ QuestionnaireService/QGen2.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SyllabaryQuizGenerator;
 
 namespace QuestionnaireService
 {
     public class QGen2
     {
-        public List<SyllabaryQuizGenerator.>
+        public List<QuizItem> GetQuizItems()
+        {
+            QuizGenerator qg = new QuizGenerator();
+            return qg.GenerateQuizItems(10);
+
+        }
     }
 }
