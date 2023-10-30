67523013cc86d9da3901b592b89c2a2b310be1e9
yevgeller <yevgeller@gmail.com>
2023-10-29 21:03:12 -0400

    Cleanup

---
+++ Hiragana1/Shared/QuizType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared
+{
+    public enum QuizType
+    {
+        EnglishToHiragana = 1,
+        EnglishToKatakana = 2,
+        HiraganaToEnglish = 3,
+        KatakanaToEnglish = 4,
+        HiraganaToKatakana = 5,
+        KatakanaToHiragana = 6,
+        Random = 7
+    }
+}
