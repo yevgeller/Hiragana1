a411082eb5136d13ea07b248381aa32f05185a90
yevgeller <yevgeller@gmail.com>
2023-10-29 21:30:19 -0400

    Wiring up quiz parameter selection

---
+++ Hiragana1/Shared/DTOs/SelectedQuizParametersDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared.DTOs
+{
+    public class SelectedQuizParametersDto
+    {
+        public QuizType QuizType { get; set; }
+        public int NumberOfQuestions { get; set; }
+    }
+}
