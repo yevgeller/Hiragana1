bf7a2e220e8a2ed03caaff387cb7da8156a7a9fb
yevgeller <yevgeller@gmail.com>
2023-11-01 22:10:07 -0400

    Adding QuizType model

---
+++ Hiragana1/Shared/Models/QuizResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared.Models
+{
+    public class QuizResult
+    {
+        public int NumberOfErrors { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public QuizType QuizType { get; set; }
+    
+        public string ResultString()
+        {
+            return $"{NumberOfQuestions} {QuizType} questions, {NumberOfErrors} errors"; ;
+    
+    }
+
+
+}
