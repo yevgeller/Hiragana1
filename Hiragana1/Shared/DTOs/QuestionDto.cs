17cb8bf7ca329559753f4289ecbfe88f04dc66ec
yevgeller <yevgeller@gmail.com>
2023-11-18 16:45:35 -0500

    Ensuring quality of test items coming back

--- Hiragana1/Shared/DTOs/QuestionDto.cs
+++ Hiragana1/Shared/DTOs/QuestionDto.cs
@@ -13,10 +13,25 @@ namespace Hiragana1.Shared.DTOs
         public int Id { get; set; } = 0;
         public int NextQuizItemId { get; set; } = 0;
         public string? Question { get; set; }
-       // public QuizType QuestionType { get; set; }
+
         public string? CorrectAnswer { get; set; }
         public List<string> Answers { get; set; } = new List<string>();
 
         public int PositionNumber { get; set; } = -1;
+
+        public bool IsCompleteQuizItem()
+        {
+            return Id > 0 && 
+                NextQuizItemId > 0 && 
+                !string.IsNullOrEmpty(Question) &&
+                !string.IsNullOrEmpty(CorrectAnswer) && 
+                Answers.Any() &&
+                !Answers.Any(x => string.IsNullOrEmpty(x));
+        }
+
+        public bool IsIncompleteQuizItem()
+        {
+            return !IsCompleteQuizItem();
+        }
     }
 }
