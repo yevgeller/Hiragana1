932cb9f89ef5a072c3e99b020bada0260b3fd9ef
yevgeller@gmail.com <yevgeller@gmail.com>
2024-02-04 20:35:10 -0500

    Renaming

---
+++ Hiragana1/Shared/DTOs/SyllabaryCharacterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared.DTOs
+{
+    public class SyllabaryCharacterDTO
+    {
+        public string Hiragana { get; set; } = string.Empty;
+        public string Katakana { get; set; } = string.Empty;
+        public string Transliteration { get; set; } = string.Empty;
+        public int UniqueId { get; set; }
+        public bool IsDigraph { get; set; } = false;
+        public bool IsWithDiacritics { get; set; } = false;
+    }
+}
