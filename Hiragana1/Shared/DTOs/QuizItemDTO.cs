4f54a1b6c66067487f9d1d02da87719d7f599885
yevgeller@gmail.com <yevgeller@gmail.com>
2024-02-04 20:35:03 -0500

    Renaming

---
+++ Hiragana1/Shared/DTOs/QuizItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiragana1.Shared.DTOs
+{
+    public class QuizItemDTO
+    {
+        public int Id { get; set; } = 0;
+        public int NextQuizItemId { get; set; } = 0;
+        public List<SyllabaryCharacterDTO> Characters { get; set; } = new List<SyllabaryCharacterDTO>();
+    }
+}
